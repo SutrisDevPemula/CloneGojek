15fdb3e9f9d602dee55e1790df93973a805a3163
SutrisDevPemula <56956940+SutrisDevPemula@users.noreply.github.com>
2021-03-27 21:41:44 +0800

    add search bar and menus

--- App.js
+++ App.js
@@ -1,15 +1,63 @@
 import React from 'react';
-import {StyleSheet, Text, View, Image} from 'react-native';
+import {StyleSheet, Text, View, Image, TextInput} from 'react-native';
 
 const App = () => {
   return (
     <View style={{flex: 1}}>
-      {/* <Text style={styles.wellcome}>Wellcome Gojek App !</Text> */}
-      <View style={{flex: 1, backgroundColor: 'blue'}}></View>
+      <View style={{flex: 1, backgroundColor: 'white'}}>
+        {/* search bar */}
+        <View
+          style={{marginHorizontal: 17, flexDirection: 'row', paddingTop: 15}}>
+          <View style={{position: 'relative', flex: 1}}>
+            <TextInput
+              placeholder="What do you want to eat?"
+              style={{
+                borderWidth: 1,
+                borderColor: '#C4C4C4',
+                borderRadius: 25,
+                height: 40,
+                fontSize: 13,
+                paddingLeft: 45,
+                paddingRight: 20,
+                backgroundColor: 'white',
+                marginRight: 18,
+              }}
+            />
+            <Image
+              source={require('./asset/icon/search.png')}
+              style={{position: 'absolute', top: 5, left: 12}}
+            />
+          </View>
 
+          <View
+            style={{width: 35, alignItems: 'center', justifyContent: 'center'}}>
+            <Image source={require('./asset/icon/promo.png')} />
+          </View>
+        </View>
+
+        {/* Gopay */}
+        <View style={{marginHorizontal: 17, marginTop: 20}}>
+          {/*Header */}
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              backgroundColor: '#2C5FB7',
+              padding: 14,
+              borderTopLeftRadius: 4,
+              borderTopRightRadius: 4
+            }}>
+            <Image source={require('./asset/icon/gopay.png')} />
+            <Text style={{color: 'white', fontSize: 16.5, fontWeight: 'bold'}}>Rp. 50.000</Text>
+          </View>
+          {/* Menu */}
+          <View></View>
+        </View>
+      </View>
+
+      {/* Bottom Navigation */}
       <View
         style={{height: 54, flexDirection: 'row', backgroundColor: 'white'}}>
-        
         <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
           <Image
             style={{width: 26, height: 26}}
@@ -19,7 +67,7 @@ const App = () => {
             Home
           </Text>
         </View>
-        
+
         <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
           <Image
             style={{width: 26, height: 26}}
@@ -29,7 +77,7 @@ const App = () => {
             Orders
           </Text>
         </View>
-        
+
         <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
           <Image
             style={{width: 26, height: 26}}
@@ -39,7 +87,7 @@ const App = () => {
             Help
           </Text>
         </View>
-        
+
         <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
           <Image
             style={{width: 26, height: 26}}
@@ -49,7 +97,7 @@ const App = () => {
             Inbox
           </Text>
         </View>
-        
+
         <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
           <Image
             style={{width: 26, height: 26}}
